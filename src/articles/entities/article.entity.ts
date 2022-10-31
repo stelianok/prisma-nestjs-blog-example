62d6f6d1c3f33ba2e77de22c550bc11290bbf69b
Kauã Steliano <stelianok@gmail.com>
2022-10-31 01:22:42 -0300

    Last Commit

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,1 +1,27 @@
-export class Article {}
+import { Article } from '@prisma/client';
+
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ArticleEntity implements Article {
+  updatedAt: Date;
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  description: string | null;
+
+  @ApiProperty()
+  body: string;
+
+  @ApiProperty()
+  published: boolean;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updateAt: Date;
+}
